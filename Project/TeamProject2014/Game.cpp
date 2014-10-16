8a16a1f83bd3dfc919f80a5e320ffe579c1357d4
pablopopcorn <checker_at_work@web.de>
2014-10-16 23:59:38 +0200

    Made the window title constant static in Game.hpp and made it an std::string (from const char*), refactored member variable names to follow convention (removed the m_-prefix), small changes

--- Project/TeamProject2014/Game.cpp
+++ Project/TeamProject2014/Game.cpp
@@ -1,15 +1,17 @@
 #include "Game.hpp"
 
+std::string Game::GAME_TITLE = "Alpha Strike";
+
 Game::Game()
 {
-	m_windowWidth = 800.f;
-	m_windowHeight = 600.f;
-	m_quit = false;
+	windowWidth = 800;
+	windowHeight = 600;
+	quit = false;
 }
-
 
 Game::~Game()
 {
+
 }
 
 /*
@@ -17,17 +19,19 @@ Game::~Game()
 */
 void Game::init()
 {
-	Uint16 contextFlags = SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL;
+	int contextFlags = SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL;
+
+	// TODO: error checking
 	int error = SDL_Init(SDL_INIT_EVERYTHING);
 
 	// Turn on double buffering.
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
 	// Create the window
-	m_window = SDL_CreateWindow(GAME_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, m_windowWidth, m_windowHeight, contextFlags);
-	m_glContext = SDL_GL_CreateContext(m_window);
+	window = SDL_CreateWindow(GAME_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, contextFlags);
+	glContext = SDL_GL_CreateContext(window);
 
-	//4:3 resolution (800 x 600)
+	// 4:3 resolution (800 x 600)
 	glOrtho(0.0, 4.f, 0.0, 3.f, -1.0, 1.0);
 }
 
@@ -36,14 +40,14 @@ void Game::eventLoop()
 {
 	SDL_Event event;
 
-	while ((!m_quit))
+	while ((!quit))
 	{
 		while (SDL_PollEvent(&event))
 		{
 			switch (event.type)
 			{
 				case SDL_QUIT:
-					m_quit = true;
+					quit = true;
 					break;
 			}
 		}
@@ -61,12 +65,12 @@ void Game::renderFrame()
 {
 	glColor3f(1.f, 0.f, 0.f);
 	glRectf(1.f, 2.f, 3.f, 1.f);
-	SDL_GL_SwapWindow(m_window);
+	SDL_GL_SwapWindow(window);
 }
 
 void Game::cleanup()
 {
-	SDL_GL_DeleteContext(m_glContext);
-	SDL_DestroyWindow(m_window);
+	SDL_GL_DeleteContext(glContext);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
--- Project/TeamProject2014/Game.hpp
+++ Project/TeamProject2014/Game.hpp
@@ -3,14 +3,17 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 
+#include <string>
+
 class Game
 {
 	private:
-		const char* GAME_TITLE = "Alpha Strike";
-		Uint16 m_windowWidth, m_windowHeight;
-		bool m_quit;
-		SDL_Window *m_window;
-		SDL_GLContext m_glContext;
+		static std::string GAME_TITLE;
+
+		int windowWidth, windowHeight;
+		bool quit;
+		SDL_Window *window;
+		SDL_GLContext glContext;
 
 	public:
 		Game();
